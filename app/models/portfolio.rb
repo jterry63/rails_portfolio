bcfa37309371d753581c92bf6e28fb99c5961be4
jterry <jordan.terry@mx.com>
2020-09-24 10:56:04 -0600

    added custom scope for angular and ruby topics

--- app/models/portfolio.rb
+++ app/models/portfolio.rb
@@ -1,3 +1,7 @@
 class Portfolio < ApplicationRecord
   validates_presence_of :title, :body, :main_image, :thumb_image
+
+
+  scope :angular, lambda { where(subtitle: "Angular") }
+  scope :ruby_on_rails, lambda { where(subtitle: "Ruby on Rails") }
 end
