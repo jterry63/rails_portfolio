dd407d65c6fae600cf645b9a7eb0d2de10eea396
jterry <jordan.terry@mx.com>
2020-09-24 11:10:33 -0600

    implemented defaults for portfolio items

--- app/models/portfolio.rb
+++ app/models/portfolio.rb
@@ -4,4 +4,12 @@ class Portfolio < ApplicationRecord
 
   scope :angular, lambda { where(subtitle: "Angular") }
   scope :ruby_on_rails, lambda { where(subtitle: "Ruby on Rails") }
+
+  after_initialize :set_defaults
+
+  def set_defaults
+    self.main_image ||= "https://placehold.it/600x400"
+    self.thumb_image ||= "https://placehold.it/350x200"
+  end
 end
+
